0f9e1d00c10d0dc17c0d3329bf95068736cac766
TonySDuarte <tonysduarte101@gmail.com>
2021-01-27 16:30:20 -0300

    Components and Style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
-function App() {
+import React, { useEffect, useState } from 'react';
+import MovieRow from './Components/MovieRow';
+import Tmdb from './Tmdb';
+
+
+export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      // Pegando toda a lista
+      let list = await Tmdb.getHomeList();
+      setMovieList(list)
+    }
+
+    loadAll()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
     </div>
   );
 }
-
-export default App;
